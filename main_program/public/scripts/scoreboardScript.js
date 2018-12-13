af38736cbd744f0cf3d8d8ecffd64829d0dce063
Adam Fraser AHCS <adamfraserahcs@gmail.com>
2018-12-13 12:17:04 +0000

    still not quite there

--- main_program/public/scripts/scoreboardScript.js
+++ main_program/public/scripts/scoreboardScript.js
@@ -5,8 +5,10 @@ const walacePointsElem = document.getElementById('walacePoints');
 const gleneaglesPointsElem = document.getElementById('gleneaglesPoints');
 const caladoniaPointsElem = document.getElementById('caladoniaPoints');
 
-const houseElems = [balmoralPointsElem, sutherlandPointsElem, gleneaglesPointsElem, caladoniaPointsElem, walacePointsElem, ramsyPointsElem];
-const houses = ['Balmoral', 'Sutherland', 'Gleneagles', 'Caladonia', 'Walace', 'Ramsy'];
+var houseElems = [balmoralPointsElem, sutherlandPointsElem, gleneaglesPointsElem, caladoniaPointsElem, walacePointsElem, ramsyPointsElem];
+var houses = ['Balmoral', 'Sutherland', 'Gleneagles', 'Caladonia', 'Walace', 'Ramsy'];
+var scores = [0,0,0,0,0,0];
+var hidden = [];
 
 const socket = io('/scoreBoardSpace');
 
@@ -20,12 +22,14 @@ socket.on('updateScores', function(data) {
     console.log(data);
     for(var i = 0; i < houseElems.length; i++) {
         houseElems[i].innerHTML = data.newScores[i];
+        scores[i] = parseInt(data.newScores[i]);
     }
 });
 
 socket.on('removeHouses', function(data) {
     for(var i = 0; i < data.houses.length; i++) {
         document.getElementById(data.houses[i].toLowerCase() + 'Div').hidden = true;
+        hidden.push(houses[i]);
     }
 });
 
@@ -34,12 +38,26 @@ socket.on('clearScoreboard', function(data) {
         document.getElementById(houses[i].toLowerCase() + 'Div').hidden = false;
         houseElems[i].innerHTML = "0"
     };
+
+    scores = [0,0,0,0,0,0];
+    hidden = [];
 });
 
 socket.on('hideRemoved', function(data) {
     for(var i = 0; i < houses.length; i++) {
         if(!(houses[i] in data.houses)) {
             document.getElementById(houses[i].toLowerCase() + 'Div').hidden = true;
+            hidden.push(houses[i]);
         }
     };
 });
+
+function sortHouses(list2Sort) {
+    //this funciton will need to sort scores and houses simultaniously
+    sortedList = [];
+
+    for(var i = 0; i < list2Sort.length; i++) {
+        //find largest and put in sorted list
+        var largest = list2Sort
+    }
+}
